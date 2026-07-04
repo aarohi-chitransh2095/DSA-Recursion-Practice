391e0861e2a94b063de97d8142338cfb927a3103
Arohi Chitransh <chitransharohi@gmail.com>
2026-07-04 22:00:05 +0530

    Create sum_of_first_n.cpp
    // Problem: Find sum of first N natural numbers using recursion
    // Description: This program calculates the sum of first N natural numbers using recursion.
    // Approach: A recursive function keeps adding numbers until it reaches the base condition.
    // Time Complexity: O(n)
    // Space Complexity: O(n)
    // Author: Arohi

---
+++ sum_of_first_n.cpp
@@ -0,0 +1,22 @@
+//sum of first n numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int sum)
+{
+    if (i < 1)
+    {
+        cout << sum;
+        return;
+    }
+
+    f(i - 1, sum + i);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    f(n, 0);
+}
