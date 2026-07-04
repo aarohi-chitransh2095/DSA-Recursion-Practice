fe1c4982adec73800f2cab4550cacb3ae10cfe57
Arohi Chitransh <chitransharohi@gmail.com>
2026-07-04 21:56:25 +0530

    Create print_1_to_n.cpp
    // Problem: Print numbers from 1 to N using recursion
    // Description: This program prints numbers from 1 to N using recursion.
    // Approach: A recursive function is called starting from 1 and continues till N.
    // Time Complexity: O(n)
    // Space Complexity: O(n)
    // Author: Arohi

---
+++ print_1_to_n.cpp
@@ -0,0 +1,20 @@
+// printing 1 to n times
+
+#include <bits/stdc++.h>
+using namespace std;
+void f(int i, int n)
+{
+    if (i > n)
+    {
+        return;
+    }
+    cout << i << endl;
+    f(i + 1, n);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    f(1, n);
+}
